8da9979075725eaea1e7adab3e022f9f9cdfaa21
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-23 08:45:01 -0500

    final

--- src/main/java/io/zipcoder/interfaces/ClassRoom.java
+++ src/main/java/io/zipcoder/interfaces/ClassRoom.java
@@ -1,5 +1,6 @@
 package io.zipcoder.interfaces;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ClassRoom {
@@ -9,10 +10,14 @@ public class ClassRoom {
         roster = new HashMap<String, Person>();
     }
     public HashMap<String,Person> getRoster(){
-        for(Instructor x: ZipCodeWilmingtonClass.getInstructors()){
+        ArrayList<Instructor> instructors = ZipCodeWilmingtonClass.getInstructors();
+        for (int i = 0; i < instructors.size(); i++) {
+            Instructor x = instructors.get(i);
             roster.put(x.getName(), x);
         }
-        for(Student y: TechConnect.getStudents()){
+        ArrayList<Student> students = TechConnect.getStudents();
+        for (int i = 0; i < students.size(); i++) {
+            Student y = students.get(i);
             roster.put(y.getName(), y);
         }
         return roster;
